a279bc8d2d2a9d671d298fb679e3b976098f6ee8
Bymbaabatdelger <145735499+Bymbaabatdelger@users.noreply.github.com>
2024-02-26 12:13:41 +0800

    cloudinary err

--- server/src/utils/cloudinary.ts
+++ server/src/utils/cloudinary.ts
@@ -1,4 +1,4 @@
-import { v2 } from "cloudinary";
+import { v2 }  from "cloudinary";
 
 export const cloudinaryConfig = async () => {
   try {
@@ -14,19 +14,22 @@ export const cloudinaryConfig = async () => {
   }
 };
 
-export const uploadToCloudinary = (path: any, folder: any) => {
-  return v2.uploader
-    .upload(path, { folder })
-    .then((data: any) => {
-      return { url: data.url, public_id: data.public_id };
-    })
-    .catch((error: any) => {
-      console.log(error);
-    });
-};
 
-export const removeFromCloudinary = async (public_id: string) => {
-  await v2.uploader.destroy(public_id, function (error: any, result: any) {
-    console.log(result, error);
-  });
-};
+
+
+// export const uploadToCloudinary = (path: any, folder: any) => {
+//   return v2.uploader
+//     .upload(path, { folder })
+//     .then((data: any) => {
+//       return { url: data.url, public_id: data.public_id };
+//     })
+//     .catch((error: any) => {
+//       console.log(error);
+//     });
+// };
+
+// export const removeFromCloudinary = async (public_id: string) => {
+//   await v2.uploader.destroy(public_id, function (error: any, result: any) {
+//     console.log(result, error);
+//   });
+// };
